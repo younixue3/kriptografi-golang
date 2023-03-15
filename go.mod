be55c85c3256f28c3513e991ef020e06a8ba2c90
Younixue3 <bussiness.needs.rctka@gmail.com>
2023-03-15 18:02:41 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module enkripsi/index
+
+go 1.20
---
+++ index.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// text adalah parameter nilai yang akan di enkripsi dan shift adalah parameter untuk jumlah langkap penggeseran nilai enkripsi
+func EncryptcaesarCipher(text string, shift int) string {
+	result := ""
+	// Perulangan Looping untuk setiap huruf
+	for _, char := range text {
+		// Validasi percabangan apakah nilai merupakan huruf kapital
+		if char >= 'A' && char <= 'Z' {
+			// Penggeseran nilai jika dibutuhkan
+			shifted := (int(char-'A') + shift) % 26
+			// Konversi nilai yang dihasilkan dari penggesran ke nilai karakter
+			result += string(rune(shifted + 'A'))
+		} else if char >= 'a' && char <= 'z' {
+			// Penggeseran nilai jika dibutuhkan
+			shifted := (int(char-'a') + shift) % 26
+			// Konversi nilai yang dihasilkan dari penggesran ke nilai karakter
+			result += string(rune(shifted + 'a'))
+		} else {
+			// Jika nilai tidak berupa huruf, maka kan di return default
+			result += string(char)
+		}
+	}
+	return result
+}
+
+func DecryptcaesarCipher(text string, shift int) string {
+	result := ""
+	// Perulangan Looping untuk setiap huruf
+	for _, char := range text {
+		// Check if the character is an uppercase letter
+		if char >= 'A' && char <= 'Z' {
+			// Apply the shift and wrap around if necessary
+			shifted := (int(char-'A') - shift + 26) % 26
+			// Convert the shifted index back to a character
+			result += string(rune(shifted + 'A'))
+		} else if char >= 'a' && char <= 'z' {
+			// Apply the shift and wrap around if necessary
+			shifted := (int(char-'a') - shift + 26) % 26
+			// Convert the shifted index back to a character
+			result += string(rune(shifted + 'a'))
+		} else {
+			// Leave the character as is if it's not a letter
+			result += string(char)
+		}
+	}
+	return result
+}
+
+func main() {
+	plaintext := "Ricko Caesar Aprilla Tiaka"
+	encrypted := EncryptcaesarCipher(plaintext, 3)
+	decrypted := DecryptcaesarCipher(encrypted, 3)
+	fmt.Println("Plain Text:", plaintext)
+	fmt.Println("Cipher Text:", encrypted)
+	fmt.Println("Decrypt Text:", decrypted)
+}
