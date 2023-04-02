fbb84d74524c9c55447f59de774e63284c4c2fd5
RICKO CAESAR APRILLA TIAKA <2011102441099@umkt.ac.id>
2023-04-02 16:44:13 +0800

    uts soal 5

--- index.go
+++ index.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 )
 
-func vigenereCipher(text string, key string) string {
+func vigenereCipher(text string, key string, decrypt bool) string {
 	var terenkripsi string
 	kunciIndex := 0
 	for _, char := range text {
 		if char >= 'a' && char <= 'z' {
-			char = (char-'a'+rune(key[kunciIndex]-'a'))%26 + 'a'
+			if decrypt {
+				char = (char-'a'-rune(key[kunciIndex]-'a')+26)%26 + 'a'
+			} else {
+				char = (char-'a'+rune(key[kunciIndex]-'a'))%26 + 'a'
+			}
 			kunciIndex = (kunciIndex + 1) % len(key)
 		} else if char >= 'A' && char <= 'Z' {
-			char = (char-'A'+rune(key[kunciIndex]-'a'))%26 + 'A'
+			if decrypt {
+				char = (char-'A'-rune(key[kunciIndex]-'a')+26)%26 + 'A'
+			} else {
+				char = (char-'A'+rune(key[kunciIndex]-'a'))%26 + 'A'
+			}
 			kunciIndex = (kunciIndex + 1) % len(key)
 		}
 		terenkripsi += string(char)
@@ -21,10 +29,10 @@ func vigenereCipher(text string, key string) string {
 }
 
 func main() {
-	text := "SELAMAT MENJALANKAN IBADAH PUASA"
-	key := "TIAKA"
-	encrypted := vigenereCipher(text, key)
-	fmt.Printf("Plaintext: %s\n", text)
+	text := "SESABIRBRPPLWGIAMIABURSPKTCKRN"
+	key := "RAHASIA"
+	decrypted := vigenereCipher(text, key, true)
+	fmt.Printf("Ciphertext: %s\n", "TEXT")
 	fmt.Printf("Key: %s\n", key)
-	fmt.Printf("Enkripsi: %s\n", encrypted)
+	fmt.Printf("Dekripsi: %s\n", decrypted)
 }
